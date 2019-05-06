020858d034a6657498cde07d884d20af492a6bff
Archana <potnis.archana@gmail.com>
2019-05-05 17:34:17 -0700

    Removed commented code and added .json

--- src/App.js
+++ src/App.js
@@ -2,12 +2,6 @@ import React, {useState, useEffect, useReducer} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Button from './components/button';
-
-// const initialXY = {
-//   x: null,
-//   y:null
-// };
-//
 const initialProfile = {
   url: null,
   followers: null,
@@ -16,43 +10,10 @@ const initialProfile = {
 
 function App() {
 
-  // const initialState = {
-  //   i:0
-  // }
-  //
-  // function reducer(state = initialState, action){
-  //   switch (action.type) {
-  //     case "increment":
-  //       return { i: state.i + 1}
-  //      case "decrement":
-  //      return { i: state.i - 1}
-  //     default:
-  //      return state
-  //   }
-  // }
-
-  //const [state, dispatch] = useReducer(reducer, initialState);
-
-  //states
-  // const[balance, setBalance] = useState(1);
-  // const[update, setUpdate]  = useState(false);
-  // const[xy, setXY] = useState(initialXY);
-
   const[profile, setProfile] = useState(initialProfile);
   //
   const getProfile = async () => {
     const response = await fetch("https://api.github.com/users/archana5075");
-
-    // const response = await fetch("https://api.github.com/users/archana5075",{
-    //      method: "POST",
-    //      mode: "cors",
-    //      cache: "no-cache",
-    //      credentials: "same-origin",
-    //      headers:{
-    //        "Content-Type": "application/json"
-    //      }
-    // })
-
     const json = await response.json();
     if(json && json.url){
       setProfile({
@@ -63,48 +24,11 @@ function App() {
     }
   }
 
- //  const clickHandlePlus = () => setBalance(balance + 1);
- //  const updateClickHandle = () => setUpdate(!update);
- //
- //  const handleMouseMove = e => {
- //   setXY({
- //      x:e.pageX,
- //      y:e.pageY
- //    });
- //  }
- //   useEffect(()=>{
- //     document.title = balance;
- //  },[update]);
- //
    useEffect(()=>{
      getProfile();
    },[]);
- //
- //   useEffect(()=>{
- //    window.addEventListener('mousemove', handleMouseMove);
- //
- //    return () => {
- //      window.removeEventListener('mousemove', handleMouseMove);
- //    }
- //
- // },[]);
 
-// Add to  return below
- // <span>{`x:${xy.x}$ y:${xy.y}$`}</span>
- // <span>{`update = ${update}$`}</span>
- //
- // <Button title="Add One Dollar" onClick={clickHandlePlus}> Add One Dollar</Button>
- // <Button title="Update" onClick={updateClickHandle}/>
- //
- // <div val={1} className="App">
- //   <header className="App-header">
- //     <span>{`i: ${state.i}$`}</span>
- //     <Button title="Increment" onClick  = {() => dispatch({type: 'increment'})} > </Button>
- //     <Button title="Decrement" onClick  = {() => dispatch({type: 'decrement'})} > </Button>
- //   </header>
- // </div>
-
-  return (
+   return (
     <div>
          <ul>
            <li> url : {profile.url} </li>
